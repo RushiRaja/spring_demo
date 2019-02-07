28f95499b11d571d539f17c55aeaeaded8f8cb63
Rushiraja Panuganti <p.rushiraja@gmail.com>
2019-02-06 21:25:22 -0600

    Added the code to for the spring boot application to say Hello World

---
+++ src/main/java/com/project/springdemo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.project.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+
+	
+	public void sayHello() {
+		System.out.println("Hello World !!!");
+	}
+
+}
--- src/main/java/com/project/springdemo/SpringdemoApplication.java
+++ src/main/java/com/project/springdemo/SpringdemoApplication.java
@@ -2,13 +2,15 @@ package com.project.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringdemoApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(SpringdemoApplication.class, args);
+		HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+		helloWorld.sayHello();
 	}
-
 }
-
