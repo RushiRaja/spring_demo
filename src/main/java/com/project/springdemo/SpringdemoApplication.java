b6e01a29cbc5cd2ebe9c7a556289c9337a382343
Rushiraja Panuganti (OSV) <rushiraja.panuganti.osv@fedex.com>
2019-02-08 13:17:25 -0600

    Added the logger to the code and added ANSI to the code

--- src/main/java/com/project/springdemo/SpringdemoApplication.java
+++ src/main/java/com/project/springdemo/SpringdemoApplication.java
@@ -1,5 +1,7 @@
 package com.project.springdemo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,14 +11,26 @@ import com.project.springdemo.controller.GreetingController;
 @SpringBootApplication
 public class SpringdemoApplication {
 
+	private static final Logger logger = LogManager.getLogger(SpringdemoApplication.class);
+	
 	public static void main(String[] args) {
 
+		logger.info("START SpringdemoApplication.main()");
+		
 		ApplicationContext ctx = SpringApplication.run(SpringdemoApplication.class, args);
 		
 		
 		GreetingController greetingController = (GreetingController) ctx.getBean("greetingController");
 		
 		System.out.println(greetingController.sayHello());
+		
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+ 
+		logger.info("END SpringdemoApplication.main()");
 
 	}
 }
