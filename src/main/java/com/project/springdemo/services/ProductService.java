e591552bd251fb826091e04e5c1e8cf495350ee0
Rushiraja Panuganti <p.rushiraja@gmail.com>
2019-02-11 01:34:36 -0600

    Updated the code for the Spring MVC

---
+++ src/main/java/com/project/springdemo/services/ProductService.java
@@ -0,0 +1,13 @@
+package com.project.springdemo.services;
+
+import java.util.List;
+
+import com.project.springdemo.domain.Product;
+
+/**
+ * @author Rushiraja
+ * 
+ */
+public interface ProductService {
+	List<Product> listAllProducts();
+}
